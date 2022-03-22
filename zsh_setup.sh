a3e44488f088413a289cd095fb7273729278d718
JackMarx <seriousfools@gmail.com>
2022-03-21 18:37:53 -0700

    initial commit

---
+++ zsh_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# from https://medium.com/@beatrizmrg/gaining-efficiency-with-iterm-prompt-customization-on-macos-3ad212f5bfde
+
+brew install iterm2 zsh zsh-completions
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
